8ca5a017ac93fd0b37f75a3c93c49dc95858b21a
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-11-22 22:27:50 +0300

    update do_create method

--- console.py
+++ console.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Console Module """
+
 import cmd
 import sys
 from models.base_model import BaseModel
@@ -73,7 +74,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] =='}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,15 +116,42 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+
+        comm_args = args.split()
+
+        if len(comm_args) < 2:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        
+        class_name = comm_args[1]
+
+        try:
+            obj_class = globals[class_name]
+        except:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
+        
+        params ={}
+        for param in comm_args[2:]:
+            key, value = param.split('=')
+
+        if value.startswith('"') and value.endswith('"'):
+            value = valuereplace('\\"', '"').replace('_', ' ')
+
+        elif '.' in value:
+            value = float(value)
+
+        else:
+            value = int(value)
+
+        params[key] = value
+
+    try:
+        obj_instance = obj_class(**params)
+        print(obj.instance.__dict__)
+
+    except Exception as e:
+        print(f"Error creating object: {e}")
         storage.save()
 
     def help_create(self):
@@ -272,7 +300,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +308,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
---
+++ tests/test_console.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+File that tests console codes
+"""
+
+import unittest
+import pep8
+import inspect
+import console
+HBNBCommand = console.HBNBCommand
+
+
+class TestDoCreateMethod(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_pep8_conformance_console(self):
+        """
+        Test console.py file conforms to PEP8.
+        """
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['console.py'])
+        self.assertEqual(
+                result.total_errors, 4,
+                "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_test_console(self):
+        """
+        Test test/test_console.py conforms to PEP8.
+        """
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_console.py'])
+        self.assertEqual(
+                result.total_errors, 0,
+                "Found code style errors (and warnings).")
+
+    def test_console_module_docstring(self):
+        """
+        Test for the console.py module docstring
+        """
+        self.assertIsNot(
+                console.__doc__, None,
+                "console.py needs a docstring")
+
+        self.assertTrue(len(
+            console.__doc__) >= 1,
+            "console.py needs a docstring")
+
+    def test_HBNBCommand_class_docstring(self):
+        """
+        Test for the HBNBCommand class docstring
+        """
+        self.assertIsNot(
+                HBNBCommand.__doc__, None,
+                "HBNBCommand class needs a docstring")
+
+        self.assertTrue(len(
+            HBNBCommand.__doc__) >= 1,
+            "HBNBCommand class needs a docstring")
