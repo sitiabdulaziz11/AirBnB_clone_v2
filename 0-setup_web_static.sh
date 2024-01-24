15d34cb3f4501377f916c2dc397f91648f207a6b
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-01-24 17:42:55 +0300

    updated deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,10 +2,12 @@
 # A Bash script that sets up my web servers for the deployment of web_static.
 sudo apt-get -y update
 sudo apt-get -y install nginx
+service nginx start
 sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
-echo "Hello world" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
+exit 0
