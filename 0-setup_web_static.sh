d36d1a958c78b1966ff7835629c2cf1b4a359467
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-02-02 11:17:27 +0300

    deployment task

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-# A Bash script that sets up my web servers for the deployment of web_static project.
+# A Bash script that sets up my web servers for the deployment of web_static.
 
 sudo apt-get -y update
 sudo apt-get -y install nginx
+service nginx start
 sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
-echo "Holberton School for the win!" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-NEW_STRING="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
-sudo sed -i "38i $NEW_STRING" /etc/nginx/sites-available/default
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
+exit 0
