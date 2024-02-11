88fbabbb54d18d74b027649a4e5167a46344e32f
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-02-11 12:08:38 +0300

    The first time for flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application:
+"""
+
+from flask import Flask
+
+# Create a Flask application
+app = Flask(__name__)
+
+# Define a route for the root URL ("/")
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    function is the view function for the root route. 
+    """
+    return 'Hello HBNB!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
